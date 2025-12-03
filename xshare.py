f2b01ad224321742bd9ac571c1b0405d478c7dc2
github xssuperxs <github 32135@qq.com>
2025-12-03 14:59:32 +0800

    XS

--- xshare.py
+++ xshare.py
@@ -306,7 +306,7 @@ class XShare:
                 # last_DIF = macd_info.macd().iloc[-1]  # 快线
                 # last_DEA = macd_info.macd_signal().iloc[-1]  # 慢线
                 last_MACD = macd_info.macd_diff().iloc[-1]  # MACD 值 红绿柱
-                if last_MACD < -0.05:
+                if last_MACD < -0.25:
                     continue
                 # 获取创新低的天数
                 sub_check_low = df_klines.iloc[curLowIndex - XShare.__NEW_LOW_DAYS: curLowIndex]
