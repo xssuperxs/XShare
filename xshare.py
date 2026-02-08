3bf19c51d103e4614ac24555cc5f7dde50cb3c65
github xssuperxs <github 32135@qq.com>
2026-02-08 18:08:05 +0800

    XS

--- xshare.py
+++ xshare.py
@@ -371,11 +371,12 @@ def _get_klines_baostock(code, period, start_date, end_date):
 def _get_trade_dates(period):
     today = datetime.date.today()
     start_date = (today - datetime.timedelta(weeks=103)).strftime('%Y-%m-%d')
+    end_date = today.strftime('%Y-%m-%d')
     rs = bs.query_history_k_data_plus(
         code='sh.000001',
         fields="date",  # 字段可调整
         start_date=start_date,  # 尽可能早的日期
-        end_date='2050-12-30',  # 未来日期确保覆盖最新数据
+        end_date=end_date,  # 未来日期确保覆盖最新数据
         frequency=period,  # d=日线，w=周线，m=月线
         adjustflag="2"  # 复权类型：3=后复权  复权类型，默认不复权：3；1：后复权；2：前复权
     )
