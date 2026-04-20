c625cf1ed34b7517d8a06001acedd9cd8e9c7bde
github xssuperxs <github 32135@qq.com>
2026-04-21 07:18:35 +0800

    XS

--- cron.py
+++ cron.py
@@ -30,6 +30,9 @@ if __name__ == '__main__':
     bs.login()
     last_trade_day = get_last_trade_date()
     bs.logout()
+    today = datetime.now().strftime('%Y-%m-%d')
+    if today != last_trade_day:
+        exit(0)
     venv_python = "/root/work/code/.venv/bin/python"
     weekday_now = datetime.now().weekday()
     period = 'd'
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import wecallback as we
 import os
-
-filepath = 'D:\\Users\\Administrator\\Desktop\\2026-04-07.txt'
-
-res = we.send_wechat_message('LiuKeSheng', filepath, 'file')
-if res.get('errcode') == 0:
-    if os.path.exists(filepath):
-        os.remove(filepath)
-else:
-    print('send_wechat_message error!')
-print(res)
+import cron
+import baostock as  bs
+cron.get_last_trade_date()
+# filepath = 'D:\\Users\\Administrator\\Desktop\\2026-04-07.txt'
+#
+# res = we.send_wechat_message('LiuKeSheng', filepath, 'file')
+# if res.get('errcode') == 0:
+#     if os.path.exists(filepath):
+#         os.remove(filepath)
+# else:
+#     print('send_wechat_message error!')
+# print(res)
+bs.login()
+print(cron.get_last_trade_date())
+bs.logout()
